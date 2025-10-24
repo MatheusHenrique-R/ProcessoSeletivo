b52a7d4a33942847db4ac9fb098878cb74a2378b
Matheus Henrique <matheushenrique.mh759@gmail.com>
2025-10-23 21:52:17 -0300

    Resolução da rota de pesquisa por id

--- controllers/ProdutoControllers.js
+++ controllers/ProdutoControllers.js
@@ -2,18 +2,35 @@ const express = require("express");
 const router = express.Router();
 const ProdutoService = require("../services/ProdutoServices");
 
-// Rota para listar todos os produtos
 router.get("/produtos", async function (req, res) {
-  try {
-    const produtos = await ProdutoService.listarProdutos();
-    res.render("api/produtos", { produtos: produtos });
-  } catch (error) {
-    req.flash("error_msg", error.message);
-    res.redirect("/api/produtos");
+  const id = req.query.id;
+
+  if (id) {
+    try {
+      const produto = await ProdutoService.buscarProdutoPorId(id);
+
+      if (produto) {
+        res.render("api/pesquisaProduto", { produto: produto });
+      } else {
+        req.flash("error_msg", "Produto não encontrado!");
+        res.redirect("/api/produtos");
+      }
+    } catch (error) {
+      req.flash("error_msg", error.message);
+      res.redirect("/api/produtos");
+    }
+  } else {
+    try {
+      console.log("Rota todos");
+      const produtos = await ProdutoService.listarProdutos();
+      res.render("api/produtos", { produtos: produtos });
+    } catch (error) {
+      req.flash("error_msg", error.message);
+      res.redirect("/api/produtos");
+    }
   }
 });
 
-// Rota para buscar um produto por ID
 router.get("/produtos/:id", async function (req, res) {
   const id = req.params.id;
 
--- models/dp.js
+++ models/dp.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 const mysql2 = require("mysql2");
-const usuario = "root";
-const senha = "Mb191216";
+/*Usuario usado para acessar o MySQL e criar o banco de dados prosel*/
+const usuario = "";
+/*Senha do usuario*/
+const senha = "";
 
 const sequelize = new Sequelize("prosel", usuario, senha, {
   host: "localhost",
